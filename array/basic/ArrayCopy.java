345e85839eb968bdf1c2e4d68e2205b63867304f
joseph <busijoseph123@gmail.com>
2024-07-12 16:02:03 +0530

    Java Array Program to Copy All the Elements of One Array to Another Array

---
+++ array/basic/ArrayCopy.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int arr[]= new int[]{1,2,3,4,5};
+        int n = arr.length;
+        int copy [] = new int[n];
+        for(int i=0;i<n;i++){
+            copy[i]=arr[i];
+        }
+        System.out.println(Arrays.toString(copy));
+    }
+}
