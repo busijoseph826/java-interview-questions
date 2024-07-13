c848bd7c193063358534002580daf7805ded7526
joseph <busijoseph123@gmail.com>
2024-07-13 21:49:35 +0530

    selection sort algorithm

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,39 +1,39 @@
-package sorting;
-import java.util.*;
-/**
- * SelectionSort
- */
 public class SelectionSort {
-    public static void main(String[] args) {
-        int arr[] =new int[]{53,44,87,8,7};
-        System.out.println("unsorted array");
-        printArray(arr);
-        sort(arr);
-        System.out.println("sorted array");
-        printArray(arr);
 
-    }
-    public static void sort(int arr[]){
+    public static void selectionSort(int[] arr) {
         int n = arr.length;
-        boolean swapped ;
-        for(int i=0;i<n-1;i++){
-            swapped=false;
-            for(int j=0;j<n-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < n-1; i++) {
+            // Find the minimum element in the unsorted array
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            if(!swapped) break;
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
     }
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Unsorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
         System.out.println();
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
-    
 }
