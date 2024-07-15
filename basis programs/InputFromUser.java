9a63df16d365a5d2c12f4baabf3dc005e721fbdf
joseph <busijoseph123@gmail.com>
2024-07-15 10:20:18 +0530

    How to Read and Print an Integer value in Java

---
+++ basis programs/InputFromUser.java
@@ -0,0 +1,14 @@
+/**
+ * InputFromUser
+ */
+import java.util.*;
+public class InputFromUser {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int num  = sc.nextInt();
+        System.out.println(num);
+        
+    }
+}
