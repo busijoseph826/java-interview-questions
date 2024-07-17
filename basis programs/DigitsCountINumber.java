83ebd8a3360d5422cc885c6efb40ee6b0145d6fb
joseph <busijoseph123@gmail.com>
2024-07-17 14:11:55 +0530

    java program to count the no digits in a number

---
+++ basis programs/DigitsCountINumber.java
@@ -0,0 +1,12 @@
+public class DigitsCountINumber {
+    public static void main(String[] args) {
+        int num = 98768;
+        int count = 0;
+        while(num!=0){
+            num = num/10;
+            count++;
+        }
+        System.out.println(count);
+        
+    }
+}
