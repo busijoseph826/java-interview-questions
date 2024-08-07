2b1750ed42bc48b1566b451d69452384b0251068
joseph busi <94781818+busijoseph826@users.noreply.github.com>
2024-08-07 12:31:36 +0530

    Merge Two Sorted Linked List

--- linked list/Main.java
+++ linked list/Main.java
@@ -189,5 +189,30 @@ public class Main
         }
 
         return false;
+	}
+	// Merge Two Sorted lists
+	public static Node merge(Node list1, Node list2) {
+        Node dummy = new Node(0);
+        Node current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
 	
 }
